fc5f5ee8f5f1991a3740dd8ef99b6102b407d709
kushal1997 <kushalrao103@gmail.com>
2023-04-22 23:25:52 +0530

    projects showcase base is ready

---
+++ src/Pages/Homepage.js
@@ -0,0 +1,210 @@
+import Img1 from "../assets/images/img1.png"
+import Img2 from "../assets/images/img2.png"
+
+export const Homepage = () => {
+  return (
+
+    <section className=" mx-auto my-auto py-7  bg-white shadow dark:bg-gray-800">
+      <div className="flex justify-center flex-wrap other:justify-evenly ">
+        <div className="max-w-2xl mx-20 my-5">
+          <div className="bg-white shadow-md border-4 border-gray-200 rounded-lg max-w-lg dark:bg-gray-800 dark:border-gray-700">
+            <a href="https://ecommerece-website-codefair-noizy.netlify.app" target="_blank">
+              <img
+                className="rounded-t-lg"
+                src={Img1}
+                alt=""
+              />
+            </a>
+            <div className="p-5">
+              <a href="https://ecommerece-website-codefair-noizy.netlify.app" target="_blank">
+                <h5 className="text-gray-900 font-bold text-2xl tracking-tight mb-2 dark:text-white">
+                  e-Commerce Website - CodeFair
+                </h5>
+              </a>
+              <p className="text-justify font-normal text-gray-700 mb-3 dark:text-gray-400">
+                An e-Commerce website with all possible functions from login, register,add to cart, payment sucess/failed to order placement and saving all of the data
+                in a temporary backend server.
+              </p>
+              <p className="text-xl font-semibold underline underline-offset-1 text-gray-700 mb-3 dark:text-gray-400">Languages Used</p>
+
+              <div className="flex flex-wrap justify-center">
+                <p className="text-white bg-gradient-to-br from-pink-500 to-orange-400 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">REACT JS</p>
+                <p className="text-white bg-gradient-to-br from-pink-500 to-orange-400 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">TAILWIND CSS</p>
+
+              </div>
+              <a
+                href="https://ecommerece-website-codefair-noizy.netlify.app" target="_blank"
+                className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-3 py-2 text-center inline-flex items-center  dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+              >
+                Visit this now
+                <svg
+                  className="-mr-1 ml-2 h-4 w-4"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+              </a>
+            </div>
+          </div>
+
+        </div>
+
+        <div className="max-w-2xl mx-20 my-5">
+          <div className="bg-white shadow-md border-4 border-gray-200 rounded-lg max-w-lg dark:bg-gray-800 dark:border-gray-700">
+            <a href="https://dhamaka-movies-noizy.netlify.app/" target="_blank">
+              <img
+                className="rounded-t-lg"
+                src={Img2}
+                alt=""
+              />
+            </a>
+            <div className="p-5">
+              <a href="https://dhamaka-movies-noizy.netlify.app/" target="_blank">
+                <h5 className="text-gray-900 font-bold text-2xl tracking-tight mb-2 dark:text-white">
+                  Dhamaka Movies
+                </h5>
+              </a>
+              <p className="text-justify font-normal text-gray-700 mb-3 dark:text-gray-400">
+                A REACT website with API calls fetches upcoming, popular, and top-rated movies data. The website is responsive and adapts to various screen sizes.
+              </p>
+              <p className="text-xl font-semibold underline underline-offset-1 text-gray-700 mb-3 dark:text-gray-400">Languages Used</p>
+
+              <div className="flex flex-wrap justify-center">
+                <p className="text-white bg-gradient-to-br from-pink-500 to-orange-400 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">REACT JS</p>
+                <p className="text-white bg-gradient-to-br from-pink-500 to-orange-400 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">TAILWIND CSS</p>
+
+              </div>
+              <a
+                href="https://dhamaka-movies-noizy.netlify.app/" target="_blank"
+                className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-3 py-2 text-center inline-flex items-center  dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+              >
+                Visit this now
+                <svg
+                  className="-mr-1 ml-2 h-4 w-4"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+              </a>
+            </div>
+          </div>
+
+        </div>
+      </div>
+
+      <div className="flex justify-center flex-wrap other:justify-evenly ">
+        <div className="max-w-2xl mx-20 my-5">
+          <div className="bg-white shadow-md border-4 border-gray-200 rounded-lg max-w-lg dark:bg-gray-800 dark:border-gray-700">
+            <a href="https://ecommerece-website-codefair-noizy.netlify.app" target="_blank">
+              <img
+                className="rounded-t-lg"
+                src={Img1}
+                alt=""
+              />
+            </a>
+            <div className="p-5">
+              <a href="https://ecommerece-website-codefair-noizy.netlify.app" target="_blank">
+                <h5 className="text-gray-900 font-bold text-2xl tracking-tight mb-2 dark:text-white">
+                  e-Commerce Website - CodeFair
+                </h5>
+              </a>
+              <p className="text-justify font-normal text-gray-700 mb-3 dark:text-gray-400">
+                An e-Commerce website with all possible functions from login, register,add to cart, payment sucess/failed to order placement and saving all of the data
+                in a temporary backend server.
+              </p>
+              <p className="text-xl font-semibold underline underline-offset-1 text-gray-700 mb-3 dark:text-gray-400">Languages Used</p>
+
+              <div className="flex flex-wrap justify-center">
+                <p className="text-white bg-gradient-to-br from-pink-500 to-orange-400 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">REACT JS</p>
+                <p className="text-white bg-gradient-to-br from-pink-500 to-orange-400 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">TAILWIND CSS</p>
+
+              </div>
+              <a
+                href="https://ecommerece-website-codefair-noizy.netlify.app" target="_blank"
+                className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-3 py-2 text-center inline-flex items-center  dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+              >
+                Visit this now
+                <svg
+                  className="-mr-1 ml-2 h-4 w-4"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+              </a>
+            </div>
+          </div>
+
+        </div>
+
+        <div className="max-w-2xl mx-20 my-5">
+          <div className="bg-white shadow-md border-4 border-gray-200 rounded-lg max-w-lg dark:bg-gray-800 dark:border-gray-700">
+            <a href="https://dhamaka-movies-noizy.netlify.app/" target="_blank">
+              <img
+                className="rounded-t-lg"
+                src={Img2}
+                alt=""
+              />
+            </a>
+            <div className="p-5">
+              <a href="https://dhamaka-movies-noizy.netlify.app/" target="_blank">
+                <h5 className="text-gray-900 font-bold text-2xl tracking-tight mb-2 dark:text-white">
+                  Dhamaka Movies
+                </h5>
+              </a>
+              <p className="text-justify font-normal text-gray-700 mb-3 dark:text-gray-400">
+                A REACT website with API calls fetches upcoming, popular, and top-rated movies data. The website is responsive and adapts to various screen sizes.
+              </p>
+              <p className="text-xl font-semibold underline underline-offset-1 text-gray-700 mb-3 dark:text-gray-400">Languages Used</p>
+
+              <div className="flex flex-wrap justify-center">
+                <p className="text-white bg-gradient-to-br from-pink-500 to-orange-400 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">REACT JS</p>
+                <p className="text-white bg-gradient-to-br from-pink-500 to-orange-400 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">TAILWIND CSS</p>
+
+              </div>
+              <a
+                href="https://dhamaka-movies-noizy.netlify.app/" target="_blank"
+                className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-3 py-2 text-center inline-flex items-center  dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+              >
+                Visit this now
+                <svg
+                  className="-mr-1 ml-2 h-4 w-4"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+              </a>
+            </div>
+          </div>
+
+        </div>
+      </div>
+
+    </section>
+
+
+  )
+}
